4b903b765be9d88b12ca503f2704bf09801ac773
Manjunath Ks <manjunath.ks@synapse.com>
2019-04-21 19:23:41 +0530

    Add first set of files

---
+++ src/jenk/test/Hello.java
@@ -0,0 +1,18 @@
+package jenk.test;
+
+public class Hello {
+	
+	
+	public void helloMsg() {
+		System.out.println("Hello, Welcome to jenkins execution..");
+	}
+	
+	public void helloMsg(int n) throws InterruptedException {
+		
+		for(int i=0; i<n ; i++) {
+			System.out.println("I'm logging for you for better testing - " + i);
+			Thread.sleep(1000);
+		}
+	}
+	
+}
---
+++ src/jenk/test/JenkMain.java
@@ -0,0 +1,16 @@
+package jenk.test;
+
+import jenk.test.Hello;
+
+public class JenkMain {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		Hello hell = new Hello();
+		hell.helloMsg();
+		hell.helloMsg(10);
+		
+	}
+	
+	
+}
